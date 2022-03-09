2515cde0ea0c8b47fedb6de0e3be3c13d486c72e
Thomas FORELL <thomas.forell@web.de>
2022-03-09 23:00:19 +0100

    Trick browser and server caches

--- bahn.js
+++ bahn.js
@@ -1,7 +1,7 @@
 function loadBahnData(requestURL, firstLane, lastLane) {
   try {
     var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
+    request.open('GET', requestURL + "?" +  Date.now().toString());
     request.responseType = 'arraybuffer';
     request.onload = writeBahn.bind(null, request, firstLane, lastLane);
     request.send();
